bcf80e6f6bdb3031b88c0c8b68e2f33516e80d9e
unknown <ozairgdsc@gmail.com>
2025-03-26 15:17:47 +0530

    APP_Development Initialized signup and login page build wiht backend functionality

---
+++ APP_DEV/app/(tabs)/TakePhoto.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TakePhoto = () => {
+  return (
+    <View>
+      <Text>TakePhoto</Text>
+    </View>
+  )
+}
+
+export default TakePhoto
+
+const styles = StyleSheet.create({})
---
+++ APP_DEV/config/fireBaseConfig.jsx
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import {getFirestore} from 'firebase/firestore';
+import { env } from "process";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: env.apiKey,
+  authDomain: env.authDomain,
+  projectId: env.projectId,
+  storageBucket: env.storageBucket,
+  messagingSenderId: env.messagingSenderId,
+  appId: env.appId,
+  measurementId: env.measurementId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+const db = getFirestore(app);
+// const analytics = getAnalytics(app);
+export { auth, db };
